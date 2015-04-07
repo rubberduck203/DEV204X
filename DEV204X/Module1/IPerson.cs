c9f4a455df84c19f1129bb8991b36ee50a6aca1a
Christopher J. McClellan <ckuhn203@gmail.com>
2015-04-07 12:10:12 -0400

    fix the repo...

---
+++ DEV204X/Module1/IPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV204X.Module1
+{
+    internal interface IPerson
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime DateOfBirth { get; set; }
+        IContactInfo ContactInfo { get; set; }
+        IList<IClass> Classes { get; set; } 
+    }
+}
