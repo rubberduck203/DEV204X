3d6b90b7f4800b8a862959389324a319b92b8d8a
Christopher J. McClellan <ckuhn203@gmail.com>
2015-04-19 12:05:25 -0400

    Added Module 5

---
+++ Module5/Models.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module5
+{
+    internal class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+
+        public Person(string first, string last, DateTime dateOfBirth)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.DateOfBirth = dateOfBirth;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", FirstName, LastName);
+        }
+    }
+
+    internal class Student : Person
+    {
+        internal Student(string first, string last, DateTime dateOfBirth)
+            : base(first, last, dateOfBirth)
+        { }
+    }
+
+    internal class Teacher : Person
+    {
+        internal Teacher(string first, string last, DateTime dateOfBirth)
+            : base(first, last, dateOfBirth)
+        { }
+    }
+
+    internal class UProgram
+    {
+        internal string Name { get; set; }
+        internal decimal RequiredCreditHours { get; set; }
+        internal List<Degree> Degrees { get; set; }
+
+        internal UProgram(string name, decimal creditHours, List<Degree> degrees)
+        {
+            this.Name = name;
+            this.RequiredCreditHours = creditHours;
+            this.Degrees = degrees;
+        }
+
+        internal UProgram(string name, decimal creditHours)
+            : this(name, creditHours, new List<Degree>())
+        { }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+    internal class Course
+    {
+        internal string Name { get; set; }
+        internal string Building { get; set; }
+        internal string Room { get; set; }
+        internal Teacher[] Teachers { get; set; }//A list would be better, but requirements call for an array.
+        internal Student[] Students { get; set; }
+
+        internal Course(string name, string building, string room, Teacher[] teachers, Student[] students)
+        {
+            this.Name = name;
+            this.Building = building;
+            this.Room = room;
+            this.Teachers = teachers;
+            this.Students = students;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+    internal enum DegreeType
+    {
+        Associate,
+        Bachelor,
+        Master,
+        Doctorate
+    }
+
+    internal class Degree
+    {
+        internal string Name { get; private set; }
+        internal DegreeType DegreeType { get; private set; }
+        internal List<Course> Courses { get; private set; }
+
+        internal Degree(string name, DegreeType type, List<Course> courses)
+        {
+            this.Name = name;
+            this.DegreeType = type;
+            this.Courses = courses;
+        }
+
+        internal Degree(string name, DegreeType type)
+            : this(name, type, new List<Course>())
+        { }
+
+        public override string ToString()
+        {
+            return String.Format("{0} of {1}", this.DegreeType, this.Name);
+        }
+    }
+}
---
+++ Module5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Module5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student1 = new Student("Joe", "Smith", new DateTime(1985, 4, 13));
+            var student2 = new Student("John", "Doe", new DateTime(1984, 1, 31));
+            var student3 = new Student("Jane", "Doe", new DateTime(1988, 9, 22));
+
+            // new Student[] {...} could be just new []{...}
+            // but I figured it's good to be explicit for the purpose of the course
+            var students = new Student[] {student1, student2, student3}; 
+
+            var teacher = new Teacher("\"Uncle\" Bob", "Martin", new DateTime(1948, 10, 8));
+
+            var cleanCode = new Course("Clean Code 101", "McConnell", "42A", new[] {teacher}, students);
+
+            var compSci = new Degree("Computer Science", DegreeType.Bachelor);
+            compSci.Courses.Add(cleanCode);
+
+            var program = new UProgram("Information Technology", 30.5m);
+            program.Degrees.Add(compSci);
+
+            WriteProgramInfo(program);
+        }
+
+        private static void WriteProgramInfo(UProgram program)
+        {
+            var degree = program.Degrees.First();
+            Console.WriteLine("The {0} program contains the {1} degree.\n", program.Name, degree);
+
+            var course = degree.Courses.First();
+            Console.WriteLine("The {0} degree contains the course {1}.\n", degree, course);
+
+            Console.WriteLine("The {0} course has {1} student(s).\n", course, course.Students.Length);
+        }
+    }
+}
