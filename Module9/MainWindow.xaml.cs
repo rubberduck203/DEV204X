786b19e3c941207e08822b6a89cb83395f2ecebf
Christopher J. McClellan <ckuhn203@gmail.com>
2015-05-03 11:06:29 -0400

    code works well enough to submit

--- Module9/MainWindow.xaml.cs
+++ Module9/MainWindow.xaml.cs
@@ -20,9 +20,67 @@ namespace Mod_9_Homework
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Student> _students;
+        private int _currentIndex;
+
+        private const int startIndex = -1; //use an out of bounds index to reset and adjust for first item added
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _students = new List<Student>();
+            _currentIndex = startIndex;
+
+            this.btnCreateStudent.Click += btnCreateStudent_Click;
+            this.btnNext.Click += btnNext_Click;
+            this.btnPrevious.Click += btnPrevious_Click;
+        }
+
+        void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            _currentIndex -= 1;
+            DisplayCurrentStudent();
+        }
+
+        void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            _currentIndex += 1;
+            DisplayCurrentStudent();
+        }
+
+        void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            _students.Add(CreateStudent());
+            ClearText();
+        }
+
+        void DisplayCurrentStudent()
+        {
+            if (_currentIndex > startIndex && _currentIndex < _students.Count)
+            {
+                DisplayStudent(_students.ElementAt(_currentIndex));
+            }
+        }
+
+        void DisplayStudent(Student student)
+        {
+            this.txtFirstName.Text = student.FistName;
+            this.txtLastName.Text = student.LastName;
+            this.txtCity.Text = student.City;
+        }
+
+        Student CreateStudent()
+        {
+            _currentIndex = startIndex; 
+            return new Student(this.txtFirstName.Text, this.txtLastName.Text, this.txtCity.Text);
+        }
+
+        void ClearText()
+        {
+            this.txtFirstName.Clear();
+            this.txtLastName.Clear();
+            this.txtCity.Clear();
         }
     }
 }
---
+++ Module9/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+    internal class Student
+    {
+        internal string FistName { get; set; }
+        internal string LastName { get; set; }
+        internal string City { get; set; }
+
+        internal Student(string first, string last, string city)
+        {
+            this.FistName = first;
+            this.LastName = last;
+            this.City = city;
+        }
+    }
+}
